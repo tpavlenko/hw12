e59992fb330dc512a2a656eca8201257402d8750
Tetiana Pavlenko <72665178+tpavlenko@users.noreply.github.com>
2021-03-24 21:52:11 +0200

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,15 +1,24 @@
 // Есть 3 input. Выводить в textarea содержимое всех полей ввода через запятую. Использовать setInterval.
 
 
-const elements = document.querySelectorAll('.input');
+let inputElements = document.querySelectorAll('input');
+let textareaElements = document.querySelector('textarea');
 
-let getContent = function () {
-	return elements;
+
+function showInput() {
+	textareaElements.value = '';
+	for (let i = 0; i < inputElements.length; i++) {
+		if (i == inputElements.length - 1) {
+			textareaElements.value += inputElements[i].value;
+		} else {
+			textareaElements.value += inputElements[i].value + ', ';
+		}
+	}
 }
 
+setInterval(showInput, 10000)
 
-setInterval(() => {
-	for (let i = 0; i < elements.length; i++) {
 
-	}
-}, 1000);
+// Вы такой вариант записи имели в виду в комментарии?
+
+// setInterval(showInput.bind(inputElements), 10000)
